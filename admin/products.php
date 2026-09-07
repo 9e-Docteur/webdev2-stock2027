4e4681f4d4f4f9e431991adf490b765327845acf
berti <berti@CLASSES.LOCAL>
2026-09-07 10:06:26 +0200

    require_once

---
+++ admin/products.php
@@ -0,0 +1,23 @@
+<?php 
+    require "../config/session.php";
+    
+    if(!isset($_SESSION['email']) || !isset($_SESSION['id'])){
+        header("Location: ../403.php");
+        exit();
+    }
+
+    require_once "../config/connexion.php";
+    require "functions.php";
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<?php include("partials/head.php"); ?>
+<body>
+    <?php include("partials/nav.php"); ?>
+    <div class="container-fluid">
+        <h1>Gestion des produits</h1>
+
+    </div>
+</body>
+</html>
